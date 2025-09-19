5e837634215782a3c9a0899aee5d9c6b18ba1f26
danyperdana <dany.ismail.perdana@gmail.com>
2025-09-19 17:38:57 +0700

    feat(factories): update UserFactory fields and add ItemFactory

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Item>
+ */
+class ItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'name' => $this->faker->name(),
+            'description' => $this->faker->sentence(),
+            'price' => $this->faker->numberBetween(1000, 10000),
+            'category_id' => $this->faker->numberBetween(1, 2),
+            'image' => $this->faker->imageUrl(),
+            'is_active' => $this->faker->boolean(),
+        ];
+    }
+}
